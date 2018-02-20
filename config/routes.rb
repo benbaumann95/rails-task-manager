961bba69057108b0504808a92a75505b75d37de4
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-02-20 13:51:45 +0000

    Made basic task manager using rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # # READ all tasks
+  # get 'tasks', to: 'tasks#index'
+  # # READ one task
+  # get 'tasks/:id', to: 'tasks#show'
+  # # CREATE one task
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
+  # # UPDATE one task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # DELETE one task
+  # delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
+
+  resources :tasks
 end
